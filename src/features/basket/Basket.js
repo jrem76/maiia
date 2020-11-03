35ede99dbfcbbe532920df88eff12f8ce9c65729
Jérémy Hébert <jeremy.jh.hebert@gmail.com>
2020-11-03 13:39:24 +0100

    feat(basket): add icon + some styling

--- src/features/basket/Basket.js
+++ src/features/basket/Basket.js
@@ -1,7 +1,23 @@
 import React from "react"
+import Badge from '@material-ui/core/Badge'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+import "./basket.css"
 
 const Basket = () => {
-    return (<div>Basket</div>)
+
+  const count = 0
+
+  return (<div className="basket__container">
+    {
+      count !== 0 && <Badge badgeContent={count} color="primary">
+      <ShoppingBasketIcon />
+    </Badge>
+    }
+
+    { count === 0 && <ShoppingBasketIcon />}
+  </div>
+  )
 }
 
 export { Basket }
