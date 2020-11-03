af8c5524ea51d8d9b28758503217345c185b0a80
Jérémy Hébert <jeremy.jh.hebert@gmail.com>
2020-11-03 14:08:31 +0100

    feat(productTable): add pagination

--- src/features/products/ProductsTable.js
+++ src/features/products/ProductsTable.js
@@ -1,7 +1,64 @@
-import React from "react"
+import React, { useEffect, useState, useCallback } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+import Pagination from '@material-ui/lab/Pagination'
+
+import {
+  addProducts,
+  getProducts,
+  getCount,
+} from './productsSlice'
+
+import "./products.css"
+
 
 const ProductsTable = () => {
-    return (<div>Products Table</div>)
+  const [page, setPage] = useState(1)
+  const dispatch = useDispatch()
+  const products = useSelector(getProducts)
+
+  const [limit, setLimit] = useState(10)
+
+  const getDatas = useCallback(async () => {
+    await fetch(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(
+        (result) => {
+          dispatch(addProducts({ products: result }))
+        },
+        // Remarque : il faut gérer les erreurs ici plutôt que dans
+        // un bloc catch() afin que nous n’avalions pas les exceptions
+        // dues à de véritables bugs dans les composants.
+        (error) => {
+          console.error(error)
+        }
+      )
+  }, [page, limit, dispatch])
+
+  useEffect(() => dispatch(getCount()), [dispatch])
+
+  useEffect(() => getDatas(), [getDatas])
+
+  console.log({
+    test: page === 1 ? 0 : (page - 1) * limit
+  })
+
+  return (<div>
+    <h1>Products Table</h1>
+    <div className="products__container">
+      {
+        products.slice(page === 1 ? 0 : (page - 1) * limit, page * limit).map((product, index) => {
+
+          return <>
+           <img src={product.thumbnailUrl} alt={product.title}/>
+          </>
+        })
+      }
+    </div>
+    <Pagination showFirstButton showLastButton variant="outlined" shape="rounded" page={page} onChange={ (event, page) => setPage(page)}/>
+  </div>)
 }
 
 export { ProductsTable }
--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -4,6 +4,7 @@ export const productsSlice = createSlice({
   name: 'products',
   initialState: {
     products: [],
+    count: 0,
   },
   reducers: {
     addProducts: (state, action) => {
@@ -15,10 +16,28 @@ export const productsSlice = createSlice({
     removeProduct: (state, action) => {
       state.value -= 1;
     },
+    setCount: (state, action) => {
+      state.count = action.payload.count
+    }
   },
 });
 
-export const { addProducts, removeProduct } = productsSlice.actions;
+export const { addProducts, removeProduct, setCount } = productsSlice.actions;
+
+export const getCount = () => async (dispatch) => {
+  await fetch(`https://jsonplaceholder.typicode.com/photos`)
+    .then(res => {
+      return res.json()
+    })
+    .then(
+      (result) => {
+        dispatch(setCount({ count: result.length }))
+      },
+      (error) => {
+        console.error(error)
+      }
+    )
+}
 
 export const selectNumberOfProducts = state => state.products.products.length;
 export const getProducts = state => state.products.products;
